c67df6d39f4e925f21e53be7676665d1aa1da620
Alba Martin <alba.martingz@gmail.com>
2024-10-31 13:52:35 +0100

    feat: add APi Swagger documentation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
-
-
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('register')
+  @ApiOperation({ summary: 'Register user' })
+  @ApiResponse({ status: 201, description: 'User created' })
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.usersService.createUser(createUserDto);
   }
