e0274181c36fb1a16cc3a0acc74bd1608cd3cb28
Alba Martin <alba.martingz@gmail.com>
2024-10-31 13:46:24 +0100

    feat: docker, database and typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+
+   TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      autoLoadEntities: true,
+    }), 
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
