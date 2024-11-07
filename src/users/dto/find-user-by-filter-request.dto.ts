03baa8c4782214018319c7626f0c13a9b99a5d8a
Alba Martin <alba.martingz@gmail.com>
2024-11-07 13:04:25 +0100

    refactor(users): remove IsEmail restriction from dto for search flexibility

--- src/users/dto/find-user-by-filter-request.dto.ts
+++ src/users/dto/find-user-by-filter-request.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsEmail, IsOptional, IsString } from 'class-validator';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 
 export class FindUserByFilterRequest {
 	@ApiProperty()
@@ -9,7 +9,6 @@ export class FindUserByFilterRequest {
 
 	@ApiProperty()
 	@IsOptional()
-	@IsEmail()
 	email?: string;
 
 	@ApiProperty()
