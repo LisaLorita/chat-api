880088d2cbf27edd8c0c861b909374992e50227f
Alba Martin <alba.martingz@gmail.com>
2024-11-11 10:25:52 +0100

    feat(messages): add entities and user one to many relations

---
+++ src/messages/entities/message.entity.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+import { UserEntity } from '../../users/entities/user.entity';
+
+@Entity('message')
+export class MessageEntity {
+	@PrimaryGeneratedColumn('uuid')
+	id: number;
+
+	@Column()
+	content: string;
+
+	@Column('uuid')
+	senderId: number;
+
+	@Column('uuid')
+	receiverId: number;
+
+	@Column()
+	createdAt: Date;
+
+	@ManyToOne(() => UserEntity, (user) => user.sentMessages)
+	@JoinColumn({ name: 'senderId' })
+	sender: UserEntity;
+
+	@ManyToOne(() => UserEntity, (user) => user.receivedMessages)
+	@JoinColumn({ name: 'receiverId' })
+	receiver: UserEntity;
+}
