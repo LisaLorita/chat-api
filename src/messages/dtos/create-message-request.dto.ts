684e929d4d7e40158d6e6658608cedf3f6b2f427
Alba Martin <alba.martingz@gmail.com>
2024-11-11 13:15:13 +0100

    refactor(messages): remove unusued constructor and static method

--- src/messages/dtos/create-message-request.dto.ts
+++ src/messages/dtos/create-message-request.dto.ts
@@ -6,19 +6,10 @@ export class CreateMessageRequest {
 	readonly content: string;
 
 	//No necesario cuando el usuario esta autenticado
+
 	@IsUUID()
 	readonly senderId: string;
 
 	@IsUUID()
 	readonly receiverId: string;
-
-	constructor(content: string, senderId: string, receiverId: string) {
-		this.content = content;
-		this.senderId = senderId;
-		this.receiverId = receiverId;
-	}
-
-	static create(content: string, senderId: string, receiverId: string): CreateMessageRequest {
-		return new CreateMessageRequest(content, senderId, receiverId);
-	}
 }
