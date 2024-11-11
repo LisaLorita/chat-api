c8fb4fb03d322ba890f9a1dd14a2f37dec6aadf2
Alba Martin <alba.martingz@gmail.com>
2024-11-11 15:47:32 +0100

    feat(messages): add guard to check if receiver is active

---
+++ src/messages/guards/check-active-user.guard.ts
@@ -0,0 +1,28 @@
+import { BadRequestException, CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { UserEntity } from '../../users/entities/user.entity';
+
+@Injectable()
+export class CheckActiveUserGuard implements CanActivate {
+	constructor(
+		@InjectRepository(UserEntity)
+		private readonly usersRepository: Repository<UserEntity>,
+	) {}
+
+	async canActivate(context: ExecutionContext): Promise<boolean> {
+		const request = context.switchToHttp().getRequest();
+		const { receiverId } = request.body;
+
+		const receiver = await this.usersRepository.findOne({
+			where: { id: receiverId, isActive: true },
+		});
+
+		if (!receiver) {
+			throw new BadRequestException('El usuario receiver no está activo');
+		}
+
+		return true;
+	}
+}
