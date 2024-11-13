eb1c1241b87837da1485b44e3a6a16a627bedce0
Alba Martin <alba.martingz@gmail.com>
2024-11-13 12:30:05 +0100

    feat(notifications): add onevent to create notification method

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -1,5 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { OnEvent } from '@nestjs/event-emitter';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { MessageCreatedEvent } from '../messages/events/message-created.event';
+import { CreateNotificationRequest } from './dtos/create-notification-request.dto';
+import { CreateNotificationResponse } from './dtos/create-notification-response.dto';
+import { NotificationEntity } from './entities/notification.entity';
 
 @Injectable()
-export class NotificationsService {}
-export default NotificationsService;
+export class NotificationsService {
+	constructor(
+		@InjectRepository(NotificationEntity)
+		private readonly notificationsRepository: Repository<NotificationEntity>,
+	) {}
+
+	@OnEvent('message.created')
+	async handleMessageCreatedEvent(event: MessageCreatedEvent): Promise<CreateNotificationResponse> {
+		console.log('Event received:', event);
+
+		const request = new CreateNotificationRequest(event.messageId);
+
+		const notification = this.notificationsRepository.create({
+			message: { id: request.messageId },
+			createdAt: new Date(),
+		});
+
+		const savedNotification = await this.notificationsRepository.save(notification);
+
+		return CreateNotificationResponse.create(
+			savedNotification.id,
+			savedNotification.message.id,
+			savedNotification.createdAt,
+		);
+	}
+}
