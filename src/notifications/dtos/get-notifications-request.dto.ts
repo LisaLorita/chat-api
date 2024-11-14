e9b8f9cd5fe90ec00486676a87c3338f938f4c33
Alba Martin <alba.martingz@gmail.com>
2024-11-14 11:42:52 +0100

    feat(notifications): add swagger documentation

--- src/notifications/dtos/get-notifications-request.dto.ts
+++ src/notifications/dtos/get-notifications-request.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsUUID } from 'class-validator';
 
 export class GetNotificationsRequest {
+	@ApiProperty()
 	@IsUUID()
 	readonly userId: string;
 
+	@ApiProperty()
 	@IsOptional()
 	readonly take?: number;
 
+	@ApiProperty()
 	@IsOptional()
 	readonly page?: number;
 }
--- src/notifications/dtos/get-notifications-response.dto.ts
+++ src/notifications/dtos/get-notifications-response.dto.ts
@@ -1,7 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 import { NotificationEntity } from '../entities/notification.entity';
 
 export class GetNotificationsResponse {
+	@ApiProperty()
 	data: NotificationEntity[];
+
+	@ApiProperty()
 	count: number;
 
 	constructor(data: NotificationEntity[], count: number) {
