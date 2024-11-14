6ab6d021f03687ffb2465f370a7ffae675940879
Alba Martin <alba.martingz@gmail.com>
2024-11-14 11:36:56 +0100

    feat(notifications): add user exists guard

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 
 import { GetNotificationsRequest } from './dtos/get-notifications-request.dto';
 import { GetNotificationsResponse } from './dtos/get-notifications-response.dto';
+import { UserExistsGuard } from './guards/user-exists.guard';
 import { NotificationsService } from './notifications.service';
 
 @Controller('notifications')
@@ -9,6 +10,7 @@ export class NotificationsController {
 	constructor(private readonly notificationsService: NotificationsService) {}
 
 	@Get(':userId')
+	@UseGuards(UserExistsGuard)
 	async get(@Param() request: GetNotificationsRequest): Promise<GetNotificationsResponse> {
 		return this.notificationsService.getByUserId(request);
 	}
--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { UsersModule } from '../users/users.module';
 import { NotificationEntity } from './entities/notification.entity';
 import NotificationsController from './notifications.controller';
 import { NotificationsService } from './notifications.service';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([NotificationEntity])],
+	imports: [TypeOrmModule.forFeature([NotificationEntity]), UsersModule],
 	controllers: [NotificationsController],
 	providers: [NotificationsService],
 	exports: [NotificationsService],
